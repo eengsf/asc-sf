3b770002311fc63eaba483d688f2eaa431f5b508
shofiyul fuad <shofiyulfuad797@gmail.com>
2024-02-26 10:57:15 +0700

    fix

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -5,44 +5,43 @@ const Contact = () => {
         <div>Hi Creative People</div>
         <div>
           <p>
-            Presenting Landing Page Design For{` `}
-            <span className="font-bold">Real Estate Website.</span>
+            Presenting Landing Page Design For
+            <span className="font-bold">-Real Estate Website.</span>
           </p>
           <p>Hope you guys enjoy it and press "L" if you like it Thanks!</p>
         </div>
         <div>
           <p>
             <span className="font-bold">Have any wonderful projects?</span>
-            {` `}
             Contact me:
           </p>
           <p>
-            Email:{` `}
+            Email:
             <span className="font-bold border-b-2 border-slate-400 cursor-pointer">
               eengsf@example.com
             </span>
           </p>
         </div>
         <div>
-          See More Interesting{` `}
+          See More Interesting-
           <span className="font-bold border-b-2 border-slate-400 cursor-pointer">
             Projects
           </span>
         </div>
         <div>
-          Follow Me On:{` `}
+          Follow Me On--
           <span className="font-bold  border-b-2 border-slate-400 cursor-pointer ">
             Instagram
           </span>
-          {` `}|{` `}
+          |
           <span className="font-bold  border-b-2 border-slate-400 cursor-pointer">
             LinkedIn
           </span>
-          {` `}|{` `}
+          |
           <span className="font-bold  border-b-2 border-slate-400 cursor-pointer">
             Behance
           </span>
-          {` `}|{` `}
+          |
           <span className="font-bold  border-b-2 border-slate-400 cursor-pointer">
             Pinterest
           </span>
